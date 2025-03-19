4ce9608b4578d3e9b32282cebb3a0335bcabda0d
alexjolt <alex@jolt.digital>
2025-03-19 11:44:25 +0000

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,25 @@
+# name: discourse-hide-topic-content
+# about: Hide topic content from guests while showing topic titles
+# version: 0.1
+# authors: Alex
+# url: https://github.com/alexjolt/discourse-hide-topic-content
+
+after_initialize do
+  require_dependency 'topic_view'
+
+  module ::HideTopicContent
+    class Engine < ::Rails::Engine
+      engine_name "hide_topic_content"
+      isolate_namespace HideTopicContent
+    end
+  end
+
+  # Modify the TopicViewSerializer
+  add_to_serializer(:topic_view, :posts, false) do
+    if scope.user.present?
+      object.posts.map { |p| p }
+    else
+      [{ cooked: "<div class='login-required'><p>You must be logged in to view this content.</p><a href='/login' class='btn btn-primary'>Log in</a></div>" }]
+    end
+  end
+end
